e818f2ff58676d6e2f648085383bb9a869abe61e
omgandhewar <gandhewarom@gmail.com>
2026-08-05 17:46:14 +0530

    initial commit

---
+++ module1.py
@@ -0,0 +1,17 @@
+name="Om"
+Age=21
+City="Nagpur"
+DOB="24-3-2005"
+college="Priyadarshini Bhagwati College of Engineering"
+Blood_Group="A+"
+
+
+print("------ My ID Card ------")
+
+print("name:",name)
+print("Age:",Age)
+print("City:",City)
+print("DOB:",DOB)
+print("Blood Group:",Blood_Group)
+
+
---
+++ module3.py
@@ -0,0 +1,13 @@
+v1 = 100
+v2 = 200
+v3 = 100
+
+print("Value of v1:", v1)
+print("Value of v2:", v2)
+print("Value of v3:", v3)
+
+print("\nMemory Address")
+
+print("id(v1):", id(v1))
+print("id(v2):", id(v2))
+print("id(v3):", id(v3))
